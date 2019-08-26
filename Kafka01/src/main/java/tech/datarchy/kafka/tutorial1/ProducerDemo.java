d1ff2a2e4783d47cf3add2fab768339db56d454b
wbelguidoum <wbelguidoum@gmail.com>
2019-08-26 20:32:44 +0200

    Producer demo

--- Kafka01/src/main/java/tech/datarchy/kafka/tutorial1/ProducerDemo.java
+++ Kafka01/src/main/java/tech/datarchy/kafka/tutorial1/ProducerDemo.java
@@ -1,4 +1,37 @@
 package tech.datarchy.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+    public static void main(String[] args) {
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic", "hello world");
+
+        // send data
+        producer.send(record);
+
+        // flush data
+        producer.flush();
+
+        // flush and close
+        producer.close();
+
+
+    }
 }
