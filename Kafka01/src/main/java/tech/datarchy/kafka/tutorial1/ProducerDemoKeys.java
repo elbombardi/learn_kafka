7e5ebad18839713d5e494e0879201337a7f84929
wbelguidoum <wbelguidoum@gmail.com>
2019-09-02 09:47:02 +0200

    CallBack and Keys

---
+++ Kafka01/src/main/java/tech/datarchy/kafka/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,56 @@
+package tech.datarchy.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+    final private static Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String bootstrapServer = "127.0.0.1:9092";
+
+        // Create producer properties :
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 100; i++) {
+            // Create a record
+            final String topic = "first_topic";
+            final String value = "value " + i;
+            final String key = "key " + (i % 3);
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, key, value);
+
+            // Send record
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successful or an exception is throw
+                    if (e == null) {
+                        // the record was successfully send
+                        logger.info("(" + key + ", " + value  + ") ==> " + recordMetadata.partition() + "\n");
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+
+                }
+            });
+
+            Thread.sleep(100);
+        }
+
+        // Flush
+        producer.flush();
+
+        // Close and flush :
+        producer.close();
+    }
+}
---
+++ Kafka01/src/main/java/tech/datarchy/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -0,0 +1,64 @@
+package tech.datarchy.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    final static private Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+    public static void main(String[] args) {
+        String bootstrapServer = "127.0.0.1:9092";
+
+        // Create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i=0; i<10; i++) {
+            // Create a record
+            ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>("first_topic","hello world " + i);
+
+            // Send data - asynchronous
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successful or an exception is throw
+
+                    if (e == null) {
+                        // the record was successfully send
+                        logger.info("Received new metadata. \n" +
+                                "Topic : " + recordMetadata.topic() + "\n" +
+                                "Partition : " + recordMetadata.partition() + "\n" +
+                                "Offset : " + recordMetadata.offset() + "\n" +
+                                "Timestamp : " + recordMetadata.timestamp());
+
+                    } else {
+                        e.printStackTrace();
+                        logger.error("Error while producing", e);
+                    }
+
+                }
+            });
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Flush data
+        producer.flush();
+
+        // Flush and close
+        producer.close();
+    }
+}
